ddce904ba5b37b998d5aebb1dc5e9b9d1b3b718b
Eric Sweigart <eric.sweigart@gmail.com>
2017-01-20 15:48:20 -0800

    Added new Class for maintaining connection state Updated the socketlistener

--- AsynchronousSocketServer/Classes/AsynchronousSocketListener.cs
+++ AsynchronousSocketServer/Classes/AsynchronousSocketListener.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsynchronousSocketServer.Classes
 {
-    class AsynchronousSocketListener
+    /// <summary>
+    /// Class which listens for a Asynchronous socket client, then hands the message to a handler
+    /// </summary>
+    public class AsynchronousSocketListener
     {
+
+        public ManualResetEvent socketaccepted = new ManualResetEvent(false);
+        public bool KeepRunning = true;
+
+        public void StartListening()
+        {
+            byte[] bytes = new Byte[1024];
+
+            //Listens on the local endpoint
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            Socket listener = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+
+                listener.Bind(localEndPoint);
+                //Accept Max 100 listeners
+                listener.Listen(100);
+
+                while (KeepRunning)
+                {
+                    //reset event to nonsignaled state
+                    socketaccepted.Reset();
+
+                    // Start an asynchronous socket to listen for connections.
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallBack),listener);
+
+                    socketaccepted.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+        public void AcceptCallBack(IAsyncResult result)
+        {
+            socketaccepted.Set();
+
+            // Get the socket that handles the client request.
+            Socket listener = (Socket)result.AsyncState;
+            Socket handler = listener.EndAccept(result);
+        }
+
     }
 }
---
+++ AsynchronousSocketServer/Classes/ConnectionState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousSocketServer.Classes
+{
+    /// <summary>
+    /// Contains the state of the various connections between the server and the clients
+    /// </summary>
+    public class ConnectionState
+    {
+        public Socket Connection = null;
+
+        public const int BufferSizw = 1024;
+
+        public byte[] buffer = new byte[BufferSizw];
+
+        public StringBuilder responsebuilder = new StringBuilder();
+
+    }
+}
